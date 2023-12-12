679b59f1559e918800e2fcc4f9b83f0c6993e505
Henrik Hofmeister <henrik@newdawn.dk>
2023-12-12 13:45:45 +0100

    clean up deps

--- webpack.config.js
+++ webpack.config.js
@@ -28,15 +28,7 @@ module.exports = {
                 loader: 'babel-loader',
                 options: {
                     sourceMaps: true,
-                    presets: ['@babel/env', '@babel/typescript', '@babel/react'],
-                    plugins: [
-                        ['@babel/plugin-proposal-decorators', { legacy: true }],
-                        ['@babel/plugin-proposal-private-methods', { loose: true }],
-                        ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
-
-                        ['@babel/plugin-proposal-class-properties', { loose: true }],
-                        '@babel/proposal-object-rest-spread',
-                    ],
+                    presets: ['@babel/env', '@babel/typescript', '@babel/react']
                 },
             },
             {
