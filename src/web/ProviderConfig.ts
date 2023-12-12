0ed711f5f2b5143706e16aae9a03efec86ba72fd
Henrik Hofmeister <henrik@newdawn.dk>
2023-12-12 19:14:34 +0100

    feat: Adds JWT authenticator implementation (#1)

---
+++ src/web/ProviderConfig.ts
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2023 Kapeta Inc.
+ * SPDX-License-Identifier: MIT
+ */
+
+import { KIND_PROVIDER, KIND_CONSUMER } from './types';
+import { IResourceTypeProvider, ResourceRole, ResourceProviderType } from '@kapeta/ui-web-types';
+import { Metadata } from '@kapeta/schemas';
+
+const packageJson = require('../../package.json');
+
+export const ProviderConfig: IResourceTypeProvider<Metadata> = {
+    kind: KIND_PROVIDER,
+    version: packageJson.version,
+    title: 'JWT Authentication Provider',
+    role: ResourceRole.PROVIDES,
+    type: ResourceProviderType.INTERNAL,
+    consumableKind: KIND_CONSUMER,
+    definition: {
+        kind: 'core/resource-type-internal',
+        metadata: {
+            name: KIND_PROVIDER,
+            title: 'REST API',
+            description: 'Provide JWT authentication to your services',
+        },
+        spec: {
+            ports: [
+                {
+                    name: 'jwt-auth',
+                    type: 'jwt-auth',
+                },
+            ],
+        },
+    },
+};
+
+export default ProviderConfig;
---
+++ src/web/types.ts
@@ -0,0 +1,8 @@
+/**
+ * Copyright 2023 Kapeta Inc.
+ * SPDX-License-Identifier: MIT
+ */
+
+export const KIND_PROVIDER = 'kapeta/resource-type-auth-jwt-provider';
+
+export const KIND_CONSUMER = 'kapeta/resource-type-auth-jwt-consumer';
